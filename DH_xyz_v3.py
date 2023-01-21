e2e9299ad5d3939465ab2a0bac8a62d0dbccf13b
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 04:17:15 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.title("Upload Data")
-collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1)
+collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=uploader_callback, key="collar_file")
 survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2)
 point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3)
 interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4)
+
+def uploader_callback():
+    st.session_state['ctr'] += 1
+    print('Uploaded file #%d' % st.session_state['ctr'])
 
 def main():
    
