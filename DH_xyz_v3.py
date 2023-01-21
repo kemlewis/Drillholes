5c243622faee9ac906a0559073ee25d9e6c2516d
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 03:03:16 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,59 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-class FileReader:
-    def __init__(self):
-        self.ENCODINGS = ["utf-8", "latin1", "iso-8859-1", "ascii"]
-        self.df_files = []
+def get_dataframe_from_csv(file):
+    return pd.read_csv(file)
 
-    def main(self):
-        st.title("File Reader")
-        i = 0
-        j = 0
-        while True:
-            file = st.file_uploader("Select a file (csv or excel)", type=["csv", "xlsx"], key=f"file_{i}")
-            i += 1
-            if file is not None:
-                df_file = self.read_file(file)
-                if df_file is not None:
-                    st.success(f"{file.name} loaded successfully!")
-                    st.write(df_file)
-                    self.df_files.append(df_file)
-            if not st.button("Add another file", key=f"add_file_{j}"):
-                break
-            j += 1
+def main():
+    st.set_page_config(page_title="CSV to Dataframe", page_icon=":chart_with_upwards_trend:", layout="wide")
+    st.title("Upload multiple CSV files")
 
+    uploaded_files = st.file_uploader("Choose a CSV file", type=["csv"], multiple=True)
 
+    if uploaded_files:
+        dataframe_dict = {}
+        for file in uploaded_files:
+            df = get_dataframe_from_csv(file)
+            dataframe_dict[file.name] = df
 
-    def read_file(self, file):
-        for encoding in self.ENCODINGS:
-            try:
-                if file.name.endswith(".csv"):
-                    data = pd.read_csv(file, encoding=encoding)
-                elif file.name.endswith(".xlsx"):
-                    data = pd.read_excel(file, encoding=encoding)
-                else:
-                    st.warning("File type not supported. Please upload a csv or excel file.")
-                    return None
-                return data
-            except Exception as e:
-                if encoding == self.ENCODINGS[-1]:
-                    st.error("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
-                    st.warning("You can try specifying the file's encoding manually.")
-                    encoding = st.selectbox("Select the file's encoding", self.ENCODINGS)
-                    if st.button("Confirm"):
-                        try:
-                            if file.name.endswith(".csv"):
-                                data = pd.read_csv(file, encoding=encoding,error_bad_lines=False)
-                            elif file.name.endswith(".xlsx"):
-                                data = pd.read_excel(file, encoding=encoding)
-                            else:
-                                st.warning("File type not supported. Please upload a csv or excel file.")
-                                return None
-                            return data
-                        except Exception as e:
-                            st.error("An error occurred while reading the file with the specified encoding. Please make sure the file is in the correct format or try a different encoding.")
+        st.write(dataframe_dict)
 
 if __name__ == "__main__":
-    file_reader = FileReader()
-    file_reader.main()
+    main()
