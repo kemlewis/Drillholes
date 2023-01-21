5715e7709bbe23cbaa97f3e739182bf410d2930e
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 05:04:10 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -10,20 +10,20 @@ def handle_file_upload(file, key):
     data = pd.read_csv(file)
     if key == 1:
         df_collar = data
+        st.dataframe(df_collar)
     elif key == 2:
         df_survey = data
     elif key == 3:
         df_point = data
     elif key == 4:
         df_interval = data
-    st.dataframe(data)
+    
 
 def main():
     collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=lambda file: handle_file_upload(file, 1))
     survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2, on_change=lambda file: handle_file_upload(file, 2))
     point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3, on_change=lambda file: handle_file_upload(file, 3))
     interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4, on_change=lambda file: handle_file_upload(file, 4))
-    st.dataframe()
 
 if __name__ == '__main__':
     main()
