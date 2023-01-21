d2e05a76db4be32560ca811b7556fc581d3d04da
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 04:58:40 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -22,6 +22,7 @@ def main():
     survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2, on_change=lambda file: handle_file_upload(file, 2))
     point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3, on_change=lambda file: handle_file_upload(file, 3))
     interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4, on_change=lambda file: handle_file_upload(file, 4))
+    st.dataframe()
 
 if __name__ == '__main__':
     main()
