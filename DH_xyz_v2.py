6e662fc48e4febd2e5f56bf9fe6e878e8dd1e335
kemlewis <keitheliotlewis@gmail.com>
2023-01-21 16:55:05 +1100

    Update DH_xyz_v2.py

--- DH_xyz_v2.py
+++ DH_xyz_v2.py
@@ -1,127 +1,118 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
-import math
-#from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+from scipy import interpolate
 
-def open_collar_file():
-    collar_file = st.file_uploader("Select Collar .csv", type=["csv"])
+def load_collar_file():
+    collar_file = st.file_uploader("Upload collar file (.csv or .xlsx)")
     if collar_file is not None:
-        collar_file_path = collar_file.name
-        st.success("Collar file uploaded: " + collar_file_path)
-        return collar_file_path
-
-def open_survey_file():
-    survey_file = st.file_uploader("Select Survey .csv", type=["csv"])
-    if survey_file is not None:
-        survey_file_path = survey_file.name
-        st.success("Survey file uploaded: " + survey_file_path)
-        return survey_file_path
-
-def drillhole_summary(df_collar, df_survey):
-    print("Number of drillholes in collar file: " + str(len(df_collar)))
-    #print("Total drillhole meterage: " + str(round(df_collar.loc[df_collar["DEPTH"] > 0, "DEPTH"].sum(),0)) + " meters")
-    print("Number of drillholes in survey file: " + str(len(df_survey['HOLEID'].unique())))
-    print("Collar file headers: " )
-    print(df_collar.columns.values)
-
-df_collar_xyz = df_collar[['HOLEID', 'DH_X', 'DH_Y', 'DH_RL']]
-df_collar_xyz=df_collar_xyz.merge(df_survey[['HOLEID','DEPTH','AZIMUTH','DIP']],how='left',on='HOLEID')
-df_collar_xyz.loc[df_collar_xyz['DEPTH'] != 0, ['DH_X', 'DH_Y', 'DH_RL']] = np.nan
-df_collar_xyz.sort_values(['HOLEID', 'DEPTH'], ascending=True)
-
-unique = df_collar_xyz['HOLEID'].unique()
-size_test = df_collar_xyz.groupby(['HOLEID']).size()
-
-# Minimum curvature formula for calcuating change in xyz from distance, dip and azi
-def calculate_xyz(depth_1, dip_1, azi_1, depth_2, dip_2, azi_2):
-    MD = depth_2 - depth_1
-    if abs(dip_1) != 90:
-        dip_1 = math.radians(90+dip_1)
-        dip_2 = math.radians(90+dip_2)
-        azi_1 = math.radians(azi_1)
-        azi_2 = math.radians(azi_2)
-
-        
-        B_rad = math.acos (math.cos(dip_2-dip_1)-(math.sin(dip_1)*math.sin(dip_2)*(1-math.cos(azi_2-azi_1))))
-        B_deg = math.degrees (B_rad)
-        if B_rad != 0:
-            RF = (2/B_rad)*math.tan(B_rad/2)
-        else:
-            RF = 0
-
-        dDepth = (MD/2)
-        dN = dDepth*((math.sin(dip_1)*math.cos(azi_1))+(math.sin(dip_2)*math.cos(azi_2)))*RF
-        dE = dDepth*((math.sin(dip_1)*math.sin(azi_1))+(math.sin(dip_2)*math.sin(azi_2)))*RF
-        dV = dDepth*(math.cos(dip_1)+math.cos(dip_2))*RF
-        #print(dDepth + dip_1 + dip_2 + RF)
-
-        return(MD, RF, dN, dE, dV)
+        collar_df = pd.read_csv(collar_file) if collar_file.endswith(".csv") else pd.read_excel(collar_file)
+        st.dataframe(collar_df.head())
+        id_col = st.selectbox("Select the column containing the drillhole ID", collar_df.columns)
+        x_col = st.selectbox("Select the column containing the X coordinate", collar_df.columns)
+        y_col = st.selectbox("Select the column containing the Y coordinate", collar_df.columns)
+        z_col = st.selectbox("Select the column containing the Z coordinate", collar_df.columns)
+        extra_cols = st.multiselect("Select any additional columns to display", collar_df.columns)
+        return collar_df, id_col, x_col, y_col, z_col, extra_cols
     else:
-        return(MD, 0, 0, 0, MD)
+        return None, None, None, None, None, None
 
-# Looping through dataframe for each hole and calculating drill trace from the collar down
-for i, val in enumerate(unique):
-    for index, row in df_collar_xyz.loc[df_collar_xyz['HOLEID']==unique[i]].iterrows():
+def load_survey_file():
+    survey_file = st.file_uploader("Upload survey file (.csv or .xlsx)")
+    if survey_file is not None:
+        survey_df = pd.read_csv(survey_file) if survey_file.endswith(".csv") else pd.read_excel(survey_file)
+        st.dataframe(survey_df.head())
+        id_col = st.selectbox("Select the column containing the drillhole ID", survey_df.columns)
+        depth_col = st.selectbox("Select the column containing the depth", survey_df.columns)
+        dip_col = st.selectbox("Select the column containing the dip", survey_df.columns)
+        azimuth_col = st.selectbox("Select the column containing the azimuth", survey_df.columns)
+        extra_cols = st.multiselect("Select any additional columns to display", survey_df.columns)
+        return survey_df, id_col, depth_col, dip_col, azimuth_col, extra_cols
+    else:
+        return None, None, None, None, None, None
 
-        if row[4] > 0:
-            results = calculate_xyz(row_previous[4], row_previous[6], row_previous[5], row[4], row[6], row[5])
+def calculate_drillhole_traces(collar_df, id_col, x_col, y_col, z_col, survey_df, id_col, depth_col, dip_col, azimuth_col):
+    traces = {}
+    for id, collar in collar_df[[id_col, x_col, y_col, z_col]].groupby(id_col):
+        survey = survey_df[survey_df[id_col] == id]
+        survey["dx"] = np.cos(np.radians(survey[dip_col])) * np.cos(np.radians(survey[azimuth_col]))
+                survey["dy"] = np.cos(np.radians(survey[dip_col])) * np.sin(np.radians(survey[azimuth_col]))
+        survey["dz"] = -np.sin(np.radians(survey[dip_col]))
+        survey["x"] = collar[x_col] + survey["dx"].cumsum()
+        survey["y"] = collar[y_col] + survey["dy"].cumsum()
+        survey["z"] = collar[z_col] + survey["dz"].cumsum()
+        traces[id] = survey[["x", "y", "z", depth_col]].values
+    return traces
 
-            df_collar_xyz.at[index, 'DH_X'] = row_previous[1] + results[3]
-            df_collar_xyz.at[index, 'DH_Y'] = row_previous[2] + results[2]
-            df_collar_xyz.at[index, 'DH_RL'] = row_previous[3] - results[4]
-            
-            df_collar_xyz.at[index, 'DH_dX'] = results[3]
-            df_collar_xyz.at[index, 'DH_dY'] = results[2]
-            df_collar_xyz.at[index, 'DH_dRL'] = results[4]
-            
-        row_previous = df_collar_xyz.iloc[index]
+def load_point_data():
+    point_file = st.file_uploader("Upload point data file (.csv or .xlsx)")
+    if point_file is not None:
+        point_df = pd.read_csv(point_file) if point_file.endswith(".csv") else pd.read_excel(point_file)
+        id_col = st.selectbox("Select the column containing the drillhole ID", point_df.columns)
+        depth_col = st.selectbox("Select the column containing the depth", point_df.columns)
+        extra_cols = st.multiselect("Select any additional columns to display", point_df.columns)
+        return point_df, id_col, depth_col, extra_cols
+    else:
+        return None, None, None, None
 
-# Formula reference: https://www.drillingformulas.com/minimum-curvature-method/
-depth_1 = 0
-depth_2 = 30
-dip_1 = math.radians(90-70.0)
-dip_2 = math.radians(90-70.4)
-azi_1 = math.radians(325)
-azi_2 = math.radians(324.9)
+def load_interval_data():
+    interval_file = st.file_uploader("Upload interval data file (.csv or .xlsx)")
+    if interval_file is not None:
+        interval_df = pd.read_csv(interval_file) if interval_file.endswith(".csv") else pd.read_excel(interval_file)
+        id_col = st.selectbox("Select the column containing the drillhole ID", interval_df.columns)
+        from_col = st.selectbox("Select the column containing the 'from' depth", interval_df.columns)
+        to_col = st.selectbox("Select the column containing the 'to' depth", interval_df.columns)
+        extra_cols = st.multiselect("Select any additional columns to display", interval_df.columns)
+        return interval_df, id_col, from_col, to_col, extra_cols
+    else:
+        return None, None, None, None, None
 
-MD = depth_2 - depth_1
-B_rad = math.acos (math.cos(dip_2-dip_1)-(math.sin(dip_1)*math.sin(dip_2)*(1-math.cos(azi_2-azi_1))))
-#B_deg = math.degrees (B_rad)
-RF = (2/B_rad)*math.tan(B_rad/2)
-    
-dDepth = (MD/2)
-dN = dDepth*((math.sin(dip_1)*math.cos(azi_1))+(math.sin(dip_2)*math.cos(azi_2)))*RF
-dE = dDepth*((math.sin(dip_1)*math.sin(azi_1))+(math.sin(dip_2)*math.sin(azi_2)))*RF
-dV = dDepth*(math.cos(dip_1)+math.cos(dip_2))*RF
+def interpolate_points(traces, point_df, id_col, depth_col):
+    points = {}
+    for id, group in point_df.groupby(id_col):
+        if id in traces:
+            f = interpolate.interp1d(traces[id][:, 3], traces[id][:, :3], axis=0, fill_value="extrapolate")
+            points[id] = f(group[depth_col]).tolist()
+    return points
 
-get_ipython().run_line_magic('matplotlib', 'qt')
+def interpolate_intervals(traces, interval_df, id_col, from_col, to_col):
+        intervals = {}
+    for id, group in interval_df.groupby(id_col):
+        if id in traces:
+            f = interpolate.interp1d(traces[id][:, 3], traces[id][:, :3], axis=0, fill_value="extrapolate")
+            intervals[id] = [f(group[from_col]), f(group[to_col])]
+    return intervals
 
-fig = plt.figure(figsize=(10, 10))
-ax = plt.axes(projection='3d')
-ax.scatter3D(df_collar_xyz['DH_X'], df_collar_xyz['DH_Y'], df_collar_xyz['DH_RL'])
-plt.show()
+def plot_drillholes(traces, points=None, intervals=None):
+    import plotly.graph_objs as go
+    data = []
+    for id, trace in traces.items():
+        data.append(go.Scatter3d(x=trace[:, 0], y=trace[:, 1], z=trace[:, 2], mode="lines", name=id))
+    if points is not None:
+        for id, group in points.items():
+            data.append(go.Scatter3d(x=[p[0] for p in group], y=[p[1] for p in group], z=[p[2] for p in group], mode="markers", name=id))
+    if intervals is not None:
+        for id, group in intervals.items():
+            data.append(go.Scatter3d(x=[p[0] for p in group], y=[p[1] for p in group], z=[p[2] for p in group], mode="lines", name=id))
+    st.plotly_chart(go.Figure(data=data))
 
+def main():
+    st.title("Drillhole Trace Calculator")
+    collar_df, id_col, x_col, y_col, z_col, extra_cols = load_collar_file()
+    survey_df, id_col, depth_col, dip_col, azimuth_col, extra_cols = load_survey_file()
+    if collar_df is not None and survey_df is not None:
+        traces = calculate_drillhole_traces(collar_df, id_col, x_col, y_col, z_col, survey_df, id_col, depth_col, dip_col, azimuth_col)
+        st.write(f"Number of drillholes: {len(traces)}")
+        st.write(f"Total drilled meterage: {sum([trace[-1, 3] for trace in traces.values()])}")
+        points = None
+        point_df, id_col, depth_col, extra_cols = load_point_data()
+        if point_df is not None:
+            points = interpolate_points(traces, point_df, id_col, depth_col)
+        intervals = None
+        interval_df, id_col, from_col, to_col, extra_cols = load_interval_data()
+        if interval_df is not None:
+            intervals = interpolate_intervals(traces, interval_df, id_col, from_col, to_col)
+        plot_drillholes(traces, points, intervals)
 
-#df_collar_xyz.to_csv('DH_xyz.csv')
-
-
-st.title("Drillhole Calculations Example")
-collar_file_path = st.button("Select Collar .csv", open_collar_file)
-survey_file_path = st.button("Select Survey .csv", open_survey_file)
-
-if collar_file_path:
-    st.write("Collar file path:", collar_file_path)
-    df_collar = pd.read_csv(collar_file_path)
-if survey_file_path:
-    st.write("Survey file path:", survey_file_path)
-    df_survey = pd.read_csv(survey_file_path)
-if st.button("Process data"):
-        drillhole_summary(df_collar, df_survey)
+if __name__ == "__main__":
+    main()
