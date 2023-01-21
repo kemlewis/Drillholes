fae25701e50696f4dae7afd816169efb568128bb
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 02:42:14 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -43,34 +43,6 @@ class FileReader:
                 return data
             except Exception as e:
                 if encoding == self.ENCODINGS[-1]:
-                    st.warning("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
-                    encoding = st.selectbox("Select the file's encoding", self.ENCODINGS, key="unique_encoding_key_"+key)
-                    if st.button("Confirm", key="confirm_"+key):
-                        try:
-                            if file.name.endswith(".csv"):
-                                data = pd.read_csv(file, encoding=encoding)
-                            elif file.name.endswith(".xlsx"):
-                                data = pd.read_excel(file, encoding=encoding)
-                            else:
-                                st.warning("File type not supported. Please upload a csv or excel file.")
-                                return None
-                            return data
-                        except Exception as e:
-                            st.error("An error occurred while reading the file
-
-    def read_file(self, file, key):
-        for encoding in self.ENCODINGS:
-            try:
-                if file.name.endswith(".csv"):
-                    data = pd.read_csv(file, encoding=encoding)
-                elif file.name.endswith(".xlsx"):
-                    data = pd.read_excel(file, encoding=encoding)
-                else:
-                    st.warning("File type not supported. Please upload a csv or excel file.")
-                    return None
-                return data
-            except Exception as e:
-                if encoding == self.ENCODINGS[-1]:
                     st.error("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
                     st.warning("You can try specifying the file's encoding manually.")
                     encoding = st.selectbox("Select the file's encoding", self.ENCODINGS, key="unique_encoding_key_"+key)
@@ -86,7 +58,6 @@ class FileReader:
                             return data
                         except Exception as e:
                             st.error("An error occurred while reading the file with the specified encoding. Please make sure the file is in the correct format or try a different encoding.")
-
 
 
 if __name__ == "__main__":
