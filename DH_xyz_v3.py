04aecac28f7eac2497a8a834b9c0d7529379d349
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 04:33:51 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -3,30 +3,32 @@ import pandas as pd
 
 st.title("Upload Data")
 
-def uploader_callback():
-    st.session_state['ctr'] += 1
-    print('Uploaded file #%d' % st.session_state['ctr'])
+def handle_file_upload(file):
+    if file = collar_file:
+        if collar_file is None:
+            collar_df = pd.read_csv(collar_file)
+            diplay_collar_df = st.dataframe(collar_df)
+    if file = survey_file:
+        if survey_file is None:
+            survey_df = pd.read_csv(survey_file)
+            diplay_survey_df = st.dataframe(survey_df)
+    if file = point_file:
+        if point_file is not None:
+            point_df = pd.read_csv(point_file)
+            st.dataframe(point_df)
+    if file = interval_file:
+        if interval_file is not None:
+            interval_df = pd.read_csv(interval_file)
+            st.dataframe(interval_df)
+        
+    
 
-def main():
+#def main():
    
-    if collar_file is None:
-        collar_df = pd.read_csv(collar_file)
-        diplay_collar_df = st.dataframe(collar_df)
-    
-    if survey_file is None:
-        survey_df = pd.read_csv(survey_file)
-        diplay_survey_df = st.dataframe(survey_df)
-    
-    if point_file is not None:
-        point_df = pd.read_csv(point_file)
-        st.dataframe(point_df)
-    
-    if interval_file is not None:
-        interval_df = pd.read_csv(interval_file)
-        st.dataframe(interval_df)
+
         
 
-collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=uploader_callback, args=key,collar_file,0)
+collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=uploader_callback)
 survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2, on_change=uploader_callback)
 point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3, on_change=uploader_callback)
 interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4, on_change=uploader_callback)
