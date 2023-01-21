5aa0774ac561549b5ba96a1627ace6dbe43deac0
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 01:24:22 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -6,12 +6,23 @@ ENCODINGS = ["utf-8", "latin1", "iso-8859-1", "ascii"]
 def main():
     st.title("File Reader")
 
-    file = st.file_uploader("Select a csv or excel file", type=["csv", "xlsx"])
+    collar_file = st.file_uploader("Select a collar file (csv or excel)", type=["csv", "xlsx"])
+    survey_file = st.file_uploader("Select a survey file (csv or excel)", type=["csv", "xlsx"])
 
-    if file is None:
-        st.warning("Please upload a file.")
+    if collar_file is None or survey_file is None:
+        st.warning("Please upload both collar and survey files.")
         return
 
+    df_collar = read_file(collar_file)
+    df_survey = read_file(survey_file)
+
+    if df_collar is None or df_survey is None:
+        st.error("An error occurred while reading the files. Please make sure the files are in the correct format or check the encoding.")
+        return
+    st.dataframe(df_collar)
+    st.dataframe(df_survey)
+
+def read_file(file):
     for encoding in ENCODINGS:
         try:
             if file.name.endswith(".csv"):
@@ -20,10 +31,8 @@ def main():
                 data = pd.read_excel(file, encoding=encoding)
             else:
                 st.warning("File type not supported. Please upload a csv or excel file.")
-                return
-
-            st.dataframe(data)
-            break
+                return None
+            return data
         except Exception as e:
             if encoding == ENCODINGS[-1]:
                 st.warning("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
@@ -36,13 +45,16 @@ def main():
                             data = pd.read_excel(file, encoding=encoding)
                         else:
                             st.warning("File type not supported. Please upload a csv or excel file.")
-                            return
-
-                        st.dataframe(data)
+                            return None
+                        return data
                     except Exception as e:
                         st.error("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
+                        return None
+                else:
+                    continue
             else:
                 continue
+
 
 if __name__ == "__main__":
     main()
