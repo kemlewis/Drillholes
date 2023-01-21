7b282b66e0bcfbb6a7572b14961c19d28a56d43b
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 05:17:01 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -8,6 +8,7 @@ def handle_file_upload(file, key):
         st.write("NONE")
         return None
     data = pd.read_csv(file)
+    st.dataframe(data)
     return data
 
 def main():
@@ -15,9 +16,13 @@ def main():
     survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2, on_change=lambda file: handle_file_upload(file, 2))
     point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3, on_change=lambda file: handle_file_upload(file, 3))
     interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4, on_change=lambda file: handle_file_upload(file, 4))
+    
     df_collar = collar_file
-    if df_collar is not None:
-        st.dataframe(df_collar)
+    df_survey = survey_file
+    df_point_file = point_file
+    df_interval_file = interval_file
+    
+    
 
 if __name__ == '__main__':
     main()
