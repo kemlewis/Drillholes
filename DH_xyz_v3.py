50171f037b76f72f97fe256087676e6257d68b69
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 03:41:31 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -6,14 +6,14 @@ def main():
     st.markdown("<h1 style='text-align:center;'>Upload Data <i class='fa fa-guardsman' aria-hidden='true'></i></h1>", unsafe_allow_html=True)
     st.markdown("<style>body{width:95%;}</style>", unsafe_allow_html=True)
 
-    collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=fu1)
+    collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1)
     if collar_file is not None:
         collar_df = pd.read_csv(collar_file)
-        diplay_collar_df = st.dataframe(collar_df, key=df1)
-    survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=fu2)
+        diplay_collar_df = st.dataframe(collar_df, key=2)
+    survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=3)
     if survey_file is not None:
         survey_df = pd.read_csv(survey_file)
-        diplay_survey_df = st.dataframe(survey_df, key=df2)
+        diplay_survey_df = st.dataframe(survey_df, key=4)
     point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"])
     if point_file is not None:
         point_df = pd.read_csv(point_file)
