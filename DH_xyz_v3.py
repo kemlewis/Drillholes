51e6916fca8db115433cbcde43cadd299313708e
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 01:08:49 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,28 +1,47 @@
 import streamlit as st
 import pandas as pd
 
+ENCODINGS = ["utf-8", "latin1", "iso-8859-1", "ascii"]
+
 def main():
     st.title("File Reader")
 
-    file = st.file_uploader("Select a file", type=["csv", "xlsx"])
+    file = st.file_uploader("Select a csv or excel file", type=["csv", "xlsx"])
 
     if file is None:
         st.warning("Please upload a file.")
         return
 
-    try:
-        if file.endswith(".csv"):
-            data = pd.read_csv(file)
-        elif file.endswith(".xlsx"):
-            data = pd.read_excel(file)
-        else:
-            st.warning("File type not supported. Please upload a csv or excel file.")
-            return
+    for encoding in ENCODINGS:
+        try:
+            if file.endswith(".csv"):
+                data = pd.read_csv(file, encoding=encoding)
+            elif file.endswith(".xlsx"):
+                data = pd.read_excel(file, encoding=encoding)
+            else:
+                st.warning("File type not supported. Please upload a csv or excel file.")
+                return
 
-        st.dataframe(data)
+            st.dataframe(data)
+            break
+        except Exception as e:
+            if encoding == ENCODINGS[-1]:
+                st.warning("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
+                encoding = st.selectbox("Select the file's encoding", ENCODINGS)
+                try:
+                    if file.endswith(".csv"):
+                        data = pd.read_csv(file, encoding=encoding)
+                    elif file.endswith(".xlsx"):
+                        data = pd.read_excel(file, encoding=encoding)
+                    else:
+                        st.warning("File type not supported. Please upload a csv or excel file.")
+                        return
 
-    except Exception as e:
-        st.error("An error occurred while reading the file. Please make sure the file is in the correct format.")
+                    st.dataframe(data)
+                except Exception as e:
+                    st.error("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
+            else:
+                continue
 
 if __name__ == "__main__":
     main()
