76756f167cc74b5ab6a56b2bc1220df2ece5468c
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 05:13:28 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -6,24 +6,18 @@ st.title("Upload Data")
 def handle_file_upload(file, key):
     if file is None:
         st.write("NONE")
-        return
+        return None
     data = pd.read_csv(file)
-    if key == 1:
-        df_collar = data
-        st.dataframe(df_collar)
-    elif key == 2:
-        df_survey = data
-    elif key == 3:
-        df_point = data
-    elif key == 4:
-        df_interval = data
-    
+    return data
 
 def main():
     collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=lambda file: handle_file_upload(file, 1))
     survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2, on_change=lambda file: handle_file_upload(file, 2))
     point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3, on_change=lambda file: handle_file_upload(file, 3))
     interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4, on_change=lambda file: handle_file_upload(file, 4))
+    df_collar = collar_file
+    if df_collar is not None:
+        st.dataframe(df_collar)
 
 if __name__ == '__main__':
     main()
