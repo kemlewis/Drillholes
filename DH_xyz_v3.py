264d2d81253da472810cdc302f86e832b42b324f
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 04:22:07 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -27,9 +27,9 @@ def main():
         
 
 collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=uploader_callback)
-survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2)
-point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3)
-interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4)
+survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2, on_change=uploader_callback)
+point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3, on_change=uploader_callback)
+interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4, on_change=uploader_callback)
 
 
 if __name__ == '__main__':
