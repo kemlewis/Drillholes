8e59a12c1ee8fe75c3b96facc690e0fdc28e9953
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 04:18:39 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.title("Upload Data")
-collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=uploader_callback)
-survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2)
-point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3)
-interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4)
 
 def uploader_callback():
     st.session_state['ctr'] += 1
@@ -28,6 +24,13 @@ def main():
     if interval_file is not None:
         interval_df = pd.read_csv(interval_file)
         st.dataframe(interval_df)
+        
+
+collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1, on_change=uploader_callback)
+survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2)
+point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3)
+interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4)
+
 
 if __name__ == '__main__':
     main()
