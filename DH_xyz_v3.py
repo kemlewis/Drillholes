fbce806196bc7fb6941da57dafd520593d652809
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 01:14:02 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -28,18 +28,19 @@ def main():
             if encoding == ENCODINGS[-1]:
                 st.warning("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
                 encoding = st.selectbox("Select the file's encoding", ENCODINGS)
-                try:
-                    if file.endswith(".csv"):
-                        data = pd.read_csv(file, encoding=encoding)
-                    elif file.endswith(".xlsx"):
-                        data = pd.read_excel(file, encoding=encoding)
-                    else:
-                        st.warning("File type not supported. Please upload a csv or excel file.")
-                        return
+                if st.button("Confirm"):
+                    try:
+                        if file.endswith(".csv"):
+                            data = pd.read_csv(file, encoding=encoding)
+                        elif file.endswith(".xlsx"):
+                            data = pd.read_excel(file, encoding=encoding)
+                        else:
+                            st.warning("File type not supported. Please upload a csv or excel file.")
+                            return
 
-                    st.dataframe(data)
-                except Exception as e:
-                    st.error("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
+                        st.dataframe(data)
+                    except Exception as e:
+                        st.error("An error occurred while reading the file. Please make sure the file is in the correct format or check the encoding.")
             else:
                 continue
 
