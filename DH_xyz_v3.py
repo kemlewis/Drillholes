af389082ddd3913ae1bfcfe169085e05cd0a1a78
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 04:05:29 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Upload Data")
 collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1)
 survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2)
 point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3)
 interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4)
 
 def main():
-    st.title("Upload Data")
-
-    
+   
     if collar_file is not None:
         collar_df = pd.read_csv(collar_file)
         diplay_collar_df = st.dataframe(collar_df)
