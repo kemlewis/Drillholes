2bc4f4d5866a125ec64738f0129d74abd774df40
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 05:52:35 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -11,16 +11,23 @@ def handle_file_upload(file):
         st.write(data)
         return data
 
-def main():
-    collar_file = st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], key=1)
-    survey_file = st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], key=2)
-    point_file = st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], key=3)
-    interval_file = st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], key=4)
+def handle_files(file_uploaders):
+    files = {}
+    dataframes = {}
+    for label, file_uploader in file_uploaders.items():
+        file = file_uploader()
+        if file:
+            files[label] = file
+            dataframes[label] = pd.read_csv(file)
+    return files, dataframes
 
-    df_collar = handle_file_upload(collar_file)
-    df_survey = handle_file_upload(survey_file)
-    df_point_file = handle_file_upload(point_file)
-    df_interval_file = handle_file_upload(interval_file)
+def main():
+    file_uploaders = {'collar': st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"]),
+                      'survey': st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"]),
+                      'point': st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"]),
+                      'interval': st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"])}
+    files, dataframes = handle_files(file_uploaders)
+
 
 if __name__ == '__main__':
     main()
