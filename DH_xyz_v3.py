f631886d17b5bda58ad0b61301d9c91dc4710fa1
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 19:19:40 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,40 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Upload Data")
+def display_dataframe(button_text):
+    uploaded_file = st.file_uploader(button_text, type=["csv"])
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        st.dataframe(data)
 
-global df_collar
-global df_survey
-
-def handle_file_upload(file, key):
-    st.write("TEST")
-    if file is None:
-        return None
-    if file is not None:
-        data = pd.read_csv(file)
-        if key == 1:
-            df_collar = data
-            st.write(df_collar)
-        elif key == 2:
-            df_survey = data
-            st.write(df_survey)
-
-def handle_files(file_uploaders):
-    files = {}
-    dataframes = {}
-    for label, file_uploader in file_uploaders.items():
-        file = file_uploader
-        if file:
-            files[label] = file
-            dataframes[label] = pd.read_csv(file)
-    return files, dataframes
-
-def main():
-    file_uploaders = {'collar': st.file_uploader("Upload collar data (csv or excel)", type=["csv", "xlsx"], on_change=lambda file: handle_file_upload(file, 1)),
-                      'survey': st.file_uploader("Upload survey data (csv or excel)", type=["csv", "xlsx"], on_change=lambda file: handle_file_upload(file, 2)),
-                      'point': st.file_uploader("Upload point data (csv or excel)", type=["csv", "xlsx"], on_change=lambda file: handle_file_upload(file, 3)),
-                      'interval': st.file_uploader("Upload interval data (csv or excel)", type=["csv", "xlsx"], on_change=lambda file: handle_file_upload(4, 4))}
-
-
-if __name__ == '__main__':
-    main()
+st.button("Upload Dataframe 1")
+display_dataframe("Dataframe 1")
+st.button("Upload Dataframe 2")
+display_dataframe("Dataframe 2")
