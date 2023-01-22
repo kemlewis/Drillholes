9f45955f9ae105ed8c08b08f401b7ee2a5253519
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 19:25:12 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-def display_dataframe(button_text):
-    if st.button(button_text):
-        uploaded_file = st.file_uploader(button_text, type=["csv"])
-        if uploaded_file is not None:
-            data = pd.read_csv(uploaded_file)
-            st.dataframe(data)
+uploaded_file1 = st.file_uploader("Upload a CSV file 1", type=["csv"])
+if uploaded_file1 is not None:
+    data1 = pd.read_csv(uploaded_file1)
+    st.dataframe(data1)
 
-display_dataframe("Upload Dataframe 1")
-display_dataframe("Upload Dataframe 2")
+uploaded_file2 = st.file_uploader("Upload a CSV file 2", type=["csv"])
+if uploaded_file2 is not None:
+    data2 = pd.read_csv(uploaded_file2)
+    st.dataframe(data2)
+
+uploaded_file3 = st.file_uploader("Upload a CSV file 3", type=["csv"])
+if uploaded_file3 is not None:
+    data3 = pd.read_csv(uploaded_file3)
+    st.dataframe(data3)
