b02107972451a6bc3d8c69a4c178f7e1a1fc056a
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 19:30:04 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -7,9 +7,9 @@ global df_survey
 uploaded_file1 = st.file_uploader("Upload a CSV file 1", type=["csv"], key=1)
 if uploaded_file1 is not None:
     df_collar = pd.read_csv(uploaded_file1)
-    st.dataframe(df_collar)
+    st.write(df_collar)
 
 uploaded_file2 = st.file_uploader("Upload a CSV file 2", type=["csv"], key=1)
 if uploaded_file2 is not None:
     df_survey = pd.read_csv(uploaded_file2)
-    st.dataframe(df_survey)
+    st.write(df_survey)
