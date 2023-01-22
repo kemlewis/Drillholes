b4c2a38ab3a8d80f2f9cfe21d728975f3323afc6
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 20:08:05 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,24 +1,38 @@
 import streamlit as st
-import pandas as pd
 
-uploaded_file1 = st.file_uploader("Upload a CSV file 1", type=["csv"])
-if uploaded_file1 is not None:
-    try:
-        data1 = pd.read_csv(uploaded_file1)
-        if data1.empty:
-            st.error("The file is empty or doesn't contain any data.")
-        else:
-            st.dataframe(data1)
-    except pd.errors.EmptyDataError:
-        st.error("The file is empty or doesn't contain any data.")
+# Create a function for each page
+def load_collar():
+    st.write("This is the Load Collar page.")
 
-uploaded_file2 = st.file_uploader("Upload a CSV file 2", type=["csv"])
-if uploaded_file2 is not None:
-    try:
-        data2 = pd.read_csv(uploaded_file2)
-        if data2.empty:
-            st.error("The file is empty or doesn't contain any data.")
-        else:
-            st.dataframe(data2)
-    except pd.errors.EmptyDataError:
-        st.error("The file is empty or doesn't contain any data.")
+def load_survey():
+    st.write("This is the Load Survey page.")
+
+def load_point_data():
+    st.write("This is the Load Point Data page.")
+
+def load_interval_data():
+    st.write("This is the Load Interval Data page.")
+
+def plot_3d():
+    st.write("This is the 3d Plot page.")
+
+# Create a function to navigate between pages
+def navigate():
+    page = st.sidebar.selectbox("Select a page", ["Load Collar", "Load Survey", "Load Point Data", "Load Interval Data", "3d Plot"])
+    if page == "Load Collar":
+        load_collar()
+    elif page == "Load Survey":
+        load_survey()
+    elif page == "Load Point Data":
+        load_point_data()
+    elif page == "Load Interval Data":
+        load_interval_data()
+    elif page == "3d Plot":
+        plot_3d()
+
+def main():
+    st.set_page_config(page_title="My App", page_icon=":guardsman:", layout="wide")
+    navigate()
+
+if __name__ == "__main__":
+    main()
