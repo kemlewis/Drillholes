e53c020da06f11e391002481e25e64f0cba51250
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 19:37:20 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -3,10 +3,22 @@ import pandas as pd
 
 uploaded_file1 = st.file_uploader("Upload a CSV file 1", type=["csv"])
 if uploaded_file1 is not None:
-    data1 = pd.read_csv(uploaded_file1)
-    st.dataframe(data1)
+    try:
+        data1 = pd.read_csv(uploaded_file1)
+        if data1.empty:
+            st.error("The file is empty or doesn't contain any data.")
+        else:
+            st.dataframe(data1)
+    except pd.errors.EmptyDataError:
+        st.error("The file is empty or doesn't contain any data.")
 
 uploaded_file2 = st.file_uploader("Upload a CSV file 2", type=["csv"])
 if uploaded_file2 is not None:
-    data2 = pd.read_csv(uploaded_file2)
-    st.dataframe(data2)
+    try:
+        data2 = pd.read_csv(uploaded_file2)
+        if data2.empty:
+            st.error("The file is empty or doesn't contain any data.")
+        else:
+            st.dataframe(data2)
+    except pd.errors.EmptyDataError:
+        st.error("The file is empty or doesn't contain any
