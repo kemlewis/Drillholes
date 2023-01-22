f22ef0bca5e0eed9f55c9e9657f520d1fa1eb337
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 19:29:19 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-uploaded_file1 = st.file_uploader("Upload a CSV file 1", type=["csv"])
-if uploaded_file1 is not None:
-    data1 = pd.read_csv(uploaded_file1)
-    st.dataframe(data1)
+global df_collar
+global df_survey
 
-uploaded_file2 = st.file_uploader("Upload a CSV file 2", type=["csv"])
+uploaded_file1 = st.file_uploader("Upload a CSV file 1", type=["csv"], key=1)
+if uploaded_file1 is not None:
+    df_collar = pd.read_csv(uploaded_file1)
+    st.dataframe(df_collar)
+
+uploaded_file2 = st.file_uploader("Upload a CSV file 2", type=["csv"], key=1)
 if uploaded_file2 is not None:
-    data2 = pd.read_csv(uploaded_file2)
-    st.dataframe(data2)
+    df_survey = pd.read_csv(uploaded_file2)
+    st.dataframe(df_survey)
