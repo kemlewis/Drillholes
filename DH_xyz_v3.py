2047565a734e98665686bbca7ccbe10bfffca79c
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 19:27:35 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -10,8 +10,3 @@ uploaded_file2 = st.file_uploader("Upload a CSV file 2", type=["csv"])
 if uploaded_file2 is not None:
     data2 = pd.read_csv(uploaded_file2)
     st.dataframe(data2)
-
-uploaded_file3 = st.file_uploader("Upload a CSV file 3", type=["csv"])
-if uploaded_file3 is not None:
-    data3 = pd.read_csv(uploaded_file3)
-    st.dataframe(data3)
