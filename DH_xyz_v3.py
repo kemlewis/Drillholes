f50cafea220ee8953198bf00c9af12b370c59ae2
kemlewis <keitheliotlewis@gmail.com>
2023-01-23 01:21:05 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -4,34 +4,31 @@ import pandas as pd
 # Initialize an empty dictionary
 uploaded_files = {}
 
+# Initialize an empty dictionary
+uploaded_files = {}
+
 def main():
     # Allow user to select file
     file = st.file_uploader("Select a file", type=["csv", "xlsx"])
     if file is not None:
         # Read the file into a DataFrame
         df = pd.read_csv(file) if file.name.endswith(".csv") else pd.read_excel(file)
-
-        #Prompt user to select category
-        file_category = st.selectbox("Select a category for the file:", ["Collar", "Survey", "Point", "Interval"])
+        if df.empty:
+            st.error("The file is empty. Please upload a valid file.")
+            return
+        # Store the file and the dataframe in the dictionary
+        uploaded_files[file.name] = {"file": file, "df": df}
         st.write(df)
-        # Confirm button
+        # Prompt user to select category
+        file_category = st.selectbox("Select a category for the file:", ["Collar", "Survey", "Point", "Interval"])
+        # Confirm and Cancel buttons
         if st.button("Confirm"):
-            if file_category == "Collar" or file_category == "Survey":
-                if file_category+"_df" in uploaded_files:
-                    st.warning("A file already exists for this category, it will be overwritten")
-                uploaded_files[file_category+"_df"] = df
-                uploaded_files[file_category+"_file"] = file
-                uploaded_files[file_category+"_category"] = file_category
-                st.success("File stored successfully")
-                # Clear the file uploader
-                st.file_uploader("Select a file", type=["csv", "xlsx"], clear_cache=True)
-            else:
-                uploaded_files[file.name] = file
-                uploaded_files[file.name + "_df"] = df
-                uploaded_files[file.name + "_category"] = file_category
-                st.success("File stored successfully")
-                # Clear the file uploader
-                st.file_uploader("Select a file", type=["csv", "xlsx"], clear_cache=True)
+            uploaded_files[file.name]["category"] = file_category
+            st.success("File stored successfully")
+        elif st.button("Cancel"):
+            del uploaded_files[file.name]
+            st.warning("File removed from the dictionary")
+            st.file_uploader("Select a file", type=["csv", "xlsx"], clear_cache=True)
 
 if __name__ == "__main__":
     main()
