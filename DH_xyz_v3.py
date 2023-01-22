828792534d82c713ff12ad49e0ad478c62fa7dcd
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 22:54:28 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -16,11 +16,13 @@ def main():
     # Allow user to select collar data file
     collar_file = st.file_uploader("Select the collar data file", type="csv", key=1)
     if collar_file is not None:
+        df_collar = None
         df_collar = pd.read_csv(collar_file, encoding='unicode_escape')
         
     # Allow user to select survey data file
     survey_file = st.file_uploader("Select the survey data file", type="csv", key=2)
     if survey_file is not None:
+        df_survey = None
         df_survey = pd.read_csv(survey_file, encoding='unicode_escape')
         
 
