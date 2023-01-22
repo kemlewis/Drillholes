41f4f301cb913b8643e4bb85c9a3797f6f289427
kemlewis <keitheliotlewis@gmail.com>
2023-01-22 23:42:41 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -8,18 +8,30 @@ def main():
     # Allow user to select file
     file = st.file_uploader("Select a file", type=["csv", "xlsx"])
     if file is not None:
-        # Add the uploaded file to the dictionary
-        uploaded_files[file.name] = file
-
         # Read the file into a DataFrame
         df = pd.read_csv(file) if file.name.endswith(".csv") else pd.read_excel(file)
 
-        # Add the DataFrame to the dictionary
-        uploaded_files[file.name + "_df"] = df
-        st.write(df)
         #Prompt user to select category
         file_category = st.selectbox("Select a category for the file:", ["Collar", "Survey", "Point", "Interval"])
-        uploaded_files[file.name + "_category"] = file_category
+        st.write(df)
+        # Confirm button
+        if st.button("Confirm"):
+            if file_category == "Collar" or file_category == "Survey":
+                if file_category+"_df" in uploaded_files:
+                    st.warning("A file already exists for this category, it will be overwritten")
+                uploaded_files[file_category+"_df"] = df
+                uploaded_files[file_category+"_file"] = file
+                uploaded_files[file_category+"_category"] = file_category
+                st.success("File stored successfully")
+                # Clear the file uploader
+                st.file_uploader("Select a file", type=["csv", "xlsx"], clear_cache=True)
+            else:
+                uploaded_files[file.name] = file
+                uploaded_files[file.name + "_df"] = df
+                uploaded_files[file.name + "_category"] = file_category
+                st.success("File stored successfully")
+                # Clear the file uploader
+                st.file_uploader("Select a file", type=["csv", "xlsx"], clear_cache=True)
 
 if __name__ == "__main__":
     main()
