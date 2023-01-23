185a327896223f19d2c9a344284b24b656cf2e86
kemlewis <keitheliotlewis@gmail.com>
2023-01-24 00:52:15 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -21,25 +21,26 @@ def main():
         else:
             df = pd.read_excel(file)
 
-        #Prompt user to select category
-        file_category = st.selectbox("Select a category for the file:", ["Collar", "Survey", "Point", "Interval"])
         st.write(df)
-        # Confirm button
-        if st.button("Confirm"):
-            if file_category == "Collar" or file_category == "Survey":
-                if file_category+"_df" in uploaded_files:
-                    st.warning("A file already exists for this category, it will be overwritten")
-                uploaded_files[file_category+"_df"] = df
-                uploaded_files[file_category+"_file"] = file
-                uploaded_files[file_category+"_category"] = file_category
-                st.success("File stored successfully")
-                st.write(uploaded_files)
-            else:
-                uploaded_files[file.name] = file
-                uploaded_files[file.name + "_df"] = df
-                uploaded_files[file.name + "_category"] = file_category
-                st.success("File stored successfully")
-                st.write(uploaded_files)
+        with st.form():
+            #Prompt user to select category
+            file_category = st.selectbox("Select a category for the file:", ["Collar", "Survey", "Point", "Interval"])
+            # Confirm button
+            if st.button("Confirm"):
+                if file_category == "Collar" or file_category == "Survey":
+                    if file_category+"_df" in uploaded_files:
+                        st.warning("A file already exists for this category, it will be overwritten")
+                    uploaded_files[file_category+"_df"] = df
+                    uploaded_files[file_category+"_file"] = file
+                    uploaded_files[file_category+"_category"] = file_category
+                    st.success("File stored successfully")
+                    st.write(uploaded_files)
+                else:
+                    uploaded_files[file.name] = file
+                    uploaded_files[file.name + "_df"] = df
+                    uploaded_files[file.name + "_category"] = file_category
+                    st.success("File stored successfully")
+                    st.write(uploaded_files)
 
 if __name__ == "__main__":
     main()
