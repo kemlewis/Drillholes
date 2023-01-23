2beca6c451465b56c41d73eeea7d1340da82fe6a
kemlewis <keitheliotlewis@gmail.com>
2023-01-24 00:59:53 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -7,40 +7,41 @@ uploaded_files = {}
 def main():
     # Allow user to select file
     df = None
-    file = st.file_uploader("Select a file", type=["csv", "xlsx"], clear_cache=True)
-    if file is not None:
-        # Read the file into a DataFrame
-        if file.name.endswith(".csv"):
-            encodings = ["utf-8", "latin1", "iso-8859-1", "ascii"]
-            for e in encodings:
-                try:
-                    df = pd.read_csv(file, encoding=e)
-                    break
-                except:
-                    continue
-        else:
-            df = pd.read_excel(file)
+    with st.form("my_form"):
+        file = st.file_uploader("Select a file", type=["csv", "xlsx"], clear_cache=True)
+        if file is not None:
+            # Read the file into a DataFrame
+            if file.name.endswith(".csv"):
+                encodings = ["utf-8", "latin1", "iso-8859-1", "ascii"]
+                for e in encodings:
+                    try:
+                        df = pd.read_csv(file, encoding=e)
+                        break
+                    except:
+                        continue
+            else:
+                df = pd.read_excel(file)
 
-        st.write(df)
-        with st.form("myform"):
             #Prompt user to select category
             file_category = st.selectbox("Select a category for the file:", ["Collar", "Survey", "Point", "Interval"])
+            st.write(df)
             # Confirm button
-            if st.button("Confirm"):
-                if file_category == "Collar" or file_category == "Survey":
-                    if file_category+"_df" in uploaded_files:
-                        st.warning("A file already exists for this category, it will be overwritten")
-                    uploaded_files[file_category+"_df"] = df
-                    uploaded_files[file_category+"_file"] = file
-                    uploaded_files[file_category+"_category"] = file_category
-                    st.success("File stored successfully")
-                    st.write(uploaded_files)
-                else:
-                    uploaded_files[file.name] = file
-                    uploaded_files[file.name + "_df"] = df
-                    uploaded_files[file.name + "_category"] = file_category
-                    st.success("File stored successfully")
-                    st.write(uploaded_files)
+    submitted = st.form_submit_button("Submit")
+       if submitted:
+            if file_category == "Collar" or file_category == "Survey":
+                if file_category+"_df" in uploaded_files:
+                    st.warning("A file already exists for this category, it will be overwritten")
+                uploaded_files[file_category+"_df"] = df
+                uploaded_files[file_category+"_file"] = file
+                uploaded_files[file_category+"_category"] = file_category
+                st.success("File stored successfully")
+                st.write(uploaded_files)
+            else:
+                uploaded_files[file.name] = file
+                uploaded_files[file.name + "_df"] = df
+                uploaded_files[file.name + "_category"] = file_category
+                st.success("File stored successfully")
+                st.write(uploaded_files)
 
 if __name__ == "__main__":
     main()
