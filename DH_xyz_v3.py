b96211c1f158957bca7fa0f8d518ed2f23636731
kemlewis <keitheliotlewis@gmail.com>
2023-01-24 21:57:56 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -78,13 +78,8 @@ def identify_columns(file_category, df):
             st.success("Columns stored successfully")
             st.write(uploaded_files)
             st.success("File has been processed successfully")
-            global uploaded_files
-            uploaded_files = {}
-            global file
             file = None
-            global df
             df = None
-            global file_category_chosen
             file_category_chosen = False
 
 
