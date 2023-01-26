5ac05cec3ab0172a730152bff671ee8be48cd22e
kemlewis <keitheliotlewis@gmail.com>
2023-01-26 19:09:57 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -40,17 +40,18 @@ def categorise_files_submit():
     st.write("FORM WAS SUBMITTED")
     
 # Create a function to handle column identification
-def identify_columns():
-    st.write("I'm trying to identify columns")
-    # Create a dropdown menu to select a file to identify columns for
-    file_select = st.selectbox("Select a file to identify columns for:", files_by_category["Collar"] + files_by_category["Survey"] + files_by_category["Point"] + files_by_category["Interval"])
+def identify_columns_form():
 
-    # Show the dataframe preview for the selected file
-    selected_file = files_dict[file_select]
-    st.dataframe(selected_file)
     
     # Create a form to select columns for the selected file based on file type
     with st.form("identify_columns"):
+        # Create a dropdown menu to select a file to identify columns for
+        file_select = st.selectbox("Select a file to identify columns for:", files_by_category["Collar"] + files_by_category["Survey"] + files_by_category["Point"] + files_by_category["Interval"])
+
+        # Show the dataframe preview for the selected file
+        selected_file = files_dict[file_select]
+        st.dataframe(selected_file)
+        
         if file_select in files_by_category["Collar"]:
             column_select = st.multiselect("Select the necessary columns for Collar file:", ["HoleID", "DH_X", "DH_Y", "DH_Z", "Depth"])
         elif file_select in files_by_category["Survey"]:
@@ -63,14 +64,16 @@ def identify_columns():
             st.warning("Invalid file type")
             return
 
-        #Submit the form
-        if st.button("Submit"):
-            # Store the column selections in the dictionary reference for the selected file
-            files_dict[file_select]["columns"] = column_select
-            # Show a success message
-            st.success("Column selections stored successfully for file: " + file_select)
+        # Submit the form and initiate view summary
+        submit_column_identification = st.form_submit_button("Submit", on_click=identify_columns_submit)
 
-            
+
+def identify_columns_submit():
+        # Store the column selections in the dictionary reference for the selected file
+        #files_dict[file_select]["columns"] = column_select
+        # Show a success message
+        st.success("Column selections stored successfully for file: " + file_select)
+    
 def view_summary():
 
     # display summary information for each file
