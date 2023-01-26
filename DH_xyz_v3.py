3ea3dce2931cac915ba2f57c8bfc1234ede29c5a
kemlewis <keitheliotlewis@gmail.com>
2023-01-26 13:03:07 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -1,88 +1,39 @@
 import streamlit as st
 import pandas as pd
 
-# Initialize an empty dictionary
-uploaded_files = {}
-file = None
-df = None
-file_category_chosen = False
-
-
 def main():
-    upload_file()
-    if df is not None:
-        st.write(df)
-        file_category = file_type_submit(df, file)
-        if file_category:
-            identify_columns(file_category, df)
-            st.success("Columns stored successfully")
+    st.set_page_config(page_title="File Upload and Processing", page_icon=":clipboard:", layout="wide")
+    st.title("File Upload and Processing")
 
+    # Define the list of predefined file types
+    file_types = ["File Type A", "File Type B", "File Type C"]
 
-    
-def upload_file():
-    # Allow user to select file
-    global df
-    global file
-    file = st.file_uploader("Select a file", type=["csv", "xlsx"])
-    if file is not None:
-        # Read the file into a DataFrame
-        if file.name.endswith(".csv"):
-            encodings = ["utf-8", "latin1", "iso-8859-1", "ascii"]
-            for e in encodings:
-                try:
-                    df = pd.read_csv(file, encoding=e)
-                    break
-                except:
-                    continue
-        else:
-            df = pd.read_excel(file)
-    
-    
-def file_type_submit(df, file):
-    with st.form(key="form_file_type_submit"):
-        #Prompt user to select category
-        file_category = st.selectbox("Select a category for the file:", ["Collar", "Survey", "Point", "Interval"])
-        # Submit form button
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            if file_category == "Collar" or file_category == "Survey":
-                if file_category+"_df" in uploaded_files:
-                    st.warning("A file already exists for this category, it will be overwritten")
-                uploaded_files[file_category+"_df"] = df
-                uploaded_files[file_category+"_file"] = file
-                uploaded_files[file_category+"_category"] = file_category
-            else:
-                uploaded_files[file.name] = file
-                uploaded_files[file.name + "_df"] = df
-                uploaded_files[file.name + "_category"] = file_category
-            return file_category
-   
+    # Allow the user to upload multiple files
+    uploaded_files = st.file_uploader("Select files to upload", type=["csv", "xlsx"], multiple=True)
 
-def identify_columns(file_category, df):
-    with st.form(key="identify_columns"):
-        selected_columns = []
-        if file_category == "Collar":
-            columns_to_identify = ["HoleID", "DH_X", "DH_Y", "DH_Z"]
-        elif file_category == "Survey":
-            columns_to_identify = ["HoleID", "Depth", "Dip", "Azimuth"]
-        elif file_category == "Point":
-            columns_to_identify = ["HoleID", "Depth"]
-        elif file_category == "Interval":
-            columns_to_identify = ["HoleID", "From", "To"]
-        else:
-            st.warning("Invalid file category")
-            return
-        for column in columns_to_identify:
-            selected_columns.append(st.selectbox(f"Select the column for {column}", df.columns))
-        # Submit form button
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            uploaded_files[file_category+"_columns"] = selected_columns
-            st.success("Columns stored successfully")
+    # Create a dictionary to store the information for each file
+    file_data = {}
 
+    # Process each uploaded file
+    for file in uploaded_files:
+        # Create a pandas dataframe from the file
+        df = pd.read_csv(file) if file.endswith(".csv") else pd.read_excel(file)
 
+        # Categorize the file based on its type
+        file_type = st.selectbox("Select the file type", file_types)
 
+        # Identify specific columns in the file
+        columns = st.multiselect("Select the columns to include", df.columns.tolist())
 
+        # Store the information for the file in the dictionary
+        file_data[file] = {"file_type": file_type, "columns": columns, "dataframe": df}
 
-if __name__ == "__main__":
+    # Display the information for each file
+    for file, data in file_data.items():
+        st.header(file)
+        st.write("File Type: ", data["file_type"])
+        st.write("Columns: ", data["columns"])
+        st.write("Dataframe: ", data["dataframe"])
+
+if __name__=="__main__":
     main()
