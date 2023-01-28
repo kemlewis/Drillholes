26c562cc74d811329782e5732a025f9194d85d3d
kemlewis <keitheliotlewis@gmail.com>
2023-01-28 18:14:38 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -101,7 +101,7 @@ def identify_columns_form(file):
                 for column in file.columns:
                     for column in file.columns:
                         default = file.simplified_dtypes.get(column) if column in file.simplified_dtypes else None
-                        option = st.selectbox(f"Select the data type for column '{column}':", options=["Not imported"] + dtypes + file.required_columns, key=column, default=default)
+                        option = st.selectbox(f"Select the data type for column '{column}':", options=file.required_columns + simplified_dtypes_options + ["Not imported"], key=column, default=default)
                     if option in file.required_columns:
                         if option in selected_options:
                             st.warning(f"{option} has already been selected. Please select a different option.")
