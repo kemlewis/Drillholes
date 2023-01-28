78b9e523f59a8434547829d35334a02610d9c5e3
kemlewis <keitheliotlewis@gmail.com>
2023-01-29 00:51:43 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -108,11 +108,12 @@ def identify_columns_form(file):
                     this_col_options = list(map(str, this_col_options))
                     #search for the item
                     this_col_index = this_col_options.index(this_col_default)
-                    option = st.selectbox(label=f"Select the data type for column '{column}':", options=this_col_options, index=this_col_index, key=file.name + "_" + column)
+                    file.columns_datatype = st.selectbox(label=f"Select the data type for column '{column}':", options=this_col_options, index=this_col_index, key=file.name + "_" + column)
                 # Submit the form and initiate view summary
                 submit_column_identification = st.form_submit_button("Submit")
                 if submit_column_identification:
                     st.success("Success")
+                    st.success(f'The {file.category} file {file.name} has had its column datatypes processed as follows: {file.columns_datatype}')
 
 def view_summary():
     # display summary information for each file
