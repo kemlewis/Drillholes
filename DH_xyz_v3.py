98e212cc6a8e53e9aa81e7293c1c9c665f378f07
kemlewis <keitheliotlewis@gmail.com>
2023-01-31 03:23:07 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import chardet
 import os
-import drillhole_calcs
+import drillhole_calcs as dh_calcs
 
 st.set_page_config(page_title="My App", page_icon=":guardsman:", layout="wide")
 
