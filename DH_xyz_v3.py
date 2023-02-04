b079ea1d92834c84b7dcb9a9861e7377685cef22
kemlewis <keitheliotlewis@gmail.com>
2023-02-04 14:31:43 +1100

    Update DH_xyz_v3.py

--- DH_xyz_v3.py
+++ DH_xyz_v3.py
@@ -61,7 +61,15 @@ def read_file_chardet(uploaded_file):
     
 def read_file_codecs_list(uploaded_file):
     file_extension = uploaded_file.name.split(".")[-1]
-    codecs = ["utf-8", "utf-16", "utf-32", "ascii"]
+    codecs = ['utf-8', 'utf-16', 'utf-32', 'ascii','big5','big5hkscs','cp037','cp273','cp424','cp437','cp500','cp720','cp737','cp775','cp850','cp852','cp855',
+            'cp856','cp857','cp858','cp860','cp861','cp862','cp863','cp864','cp865','cp866','cp869','cp874','cp875','cp932','cp949',
+            'cp950','cp1006','cp1026','cp1125','cp1140','cp1250','cp1251','cp1252','cp1253','cp1254','cp1255','cp1256','cp1257','cp1258',
+            'euc_jp','euc_jis_2004','euc_jisx0213','euc_kr','gb2312','gbk','gb18030','hz','iso2022_jp','iso2022_jp_1','iso2022_jp_2',
+            'iso2022_jp_2004','iso2022_jp_3','iso2022_jp_ext','iso2022_kr','latin_1','iso8859_2','iso8859_3','iso8859_4','iso8859_5','iso8859_6',
+            'iso8859_7','iso8859_8','iso8859_9','iso8859_10','iso8859_11','iso8859_13','iso8859_14','iso8859_15','iso8859_16','johab','koi8_r','koi8_t',
+            'koi8_u','kz1048','mac_cyrillic','mac_greek','mac_iceland','mac_latin2','mac_roman','mac_turkish','ptcp154','shift_jis','shift_jis_2004',
+            'shift_jisx0213','utf_32','utf_32_be','utf_32_le','utf_16','utf_16_be','utf_16_le','utf_7','utf_8','utf_8_sig']
+    
     try:
         if file_extension in ["csv", "txt"]:
             for codec in codecs:
@@ -330,9 +338,9 @@ def upload_files():
                             except Exception as e:
                                 st.warning(f"Pandas default pd.read_csv and pd.read_excel failed to read {uploaded_file.name}")
                         except Exception as e:
-                            st.warning(f"Pandas default pd.read_csv and pd.read_excel failed to read {uploaded_file.name}")
+                            st.warning(f"Chardet failed to read encoding for {uploaded_file.name}")
                     except Exception as e:
-                        st.warning(f"Pandas default pd.read_csv and pd.read_excel failed to read {uploaded_file.name}")
+                        st.warning(f"Pandas failed to read file using list of codecs {uploaded_file.name}")
                     if uploaded_file_df is None:
                         st.warning(f"{uploaded_file.name} was unable to be loaded.")
                     else:
